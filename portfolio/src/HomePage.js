36f6f591063c157a6d5b223456e58643d4b2b71c
Giorgi Barishvili <90037916+giorgibarishvili@users.noreply.github.com>
2024-05-27 21:42:58 +0400

    editing contacting page

--- portfolio/src/HomePage.js
+++ portfolio/src/HomePage.js
@@ -8,37 +8,86 @@ import { useTranslation } from "react-i18next";
 import { useState } from "react";
 function HomePage() {
   const [messageBox, setMessageBox] = useState("");
-
+  const [userName, setUserName] = useState("");
+  const [eMail, setEMail] = useState("");
   const { t } = useTranslation();
   return (
     <div className="container main">
       {/* <EmblaCarousel /> */}
       <div className="who-am-i mt-5">
-        <Appear>
-          {t("greetings")}
-          <br />
-          {t("description")}
-        </Appear>
+        <div style={{ maxWidth: "652px" }}>
+          <Appear>
+            {t("greetings")}
+            <br />
+            {t("description")}
+          </Appear>
+        </div>
         <Reveal>
           <Programmer />
         </Reveal>
       </div>
-      <div>
-        <Appear>
-          <input
-            style={{
-              height: "400px",
-              width: "500px",
-              // margin: "auto",
-              backgroundColor: "black",
-              color: "white",
-            }}
-            //aq gavcherdi inputebis damatebaze
-            className={"form-control active-input mx-auto"}
-            value={messageBox}
-            onChange={(e) => setMessageBox(e.target.value)}
-          ></input>
-        </Appear>
+      <div className="form d-flex">
+        <div className="me-5">
+          lets connect <br />
+          my email is asdas@aSd,.asr
+        </div>
+        <form className="ms-5">
+          <Appear>
+            <div className="form-floating input-frame pb-5">
+              <input
+                type="text"
+                className={"form-control"}
+                id="floatingInput"
+                value={userName}
+                placeholder="Input"
+                onChange={(e) => setUserName(e.target.value)}
+                autoComplete="off"
+              />
+              <label htmlFor="floatingInput">Your name</label>
+            </div>
+          </Appear>
+
+          <Appear>
+            <div className="form-floating input-frame pb-5">
+              <input
+                type="text"
+                className={"form-control"}
+                id="floatingInput"
+                value={eMail}
+                placeholder="Input"
+                onChange={(e) => setEMail(e.target.value)}
+                autoComplete="off"
+              />
+              <label htmlFor="floatingInput">Your EMail</label>
+            </div>
+          </Appear>
+          <Appear>
+            <div className="form-floating input-frame pb-5">
+              <textarea
+                className="input-field textarea form-floating form-control"
+                id="floatingInput"
+                placeholder={t("post")}
+                value={messageBox}
+                onChange={(e) => setMessageBox(e.target.value)}
+                required
+              ></textarea>
+              <label htmlFor="floatingInput">Enter text</label>
+            </div>
+          </Appear>
+          {/* <input
+              style={{
+                height: "400px",
+                width: "500px",
+                // margin: "auto",
+                backgroundColor: "black",
+                color: "white",
+              }}
+              //aq gavcherdi inputebis damatebaze
+              className={"form-control active-input mx-auto"}
+              value={messageBox}
+              onChange={(e) => setMessageBox(e.target.value)}
+            ></input> */}
+        </form>
       </div>
     </div>
   );
